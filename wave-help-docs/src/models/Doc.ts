e1db2b8afa8e02a78ee76c07b9c01e8b9363da53
Sara Baqla <missatrox44@gmail.com>
2023-04-13 12:00:50 -0500

    migrated controllers, helpers, hooks and models

---
+++ wave-help-docs/src/models/Doc.ts
@@ -0,0 +1,21 @@
+import { Document } from "@contentful/rich-text-types";
+import { Entry } from "contentful";
+import Audience from "./Audience";
+import { objectValidate } from "./Object";
+import { stringValidate } from "./String";
+
+export default interface Doc {
+  title: string;
+  subtitle?: string;
+  body: Document;
+  audience: Entry<Audience>;
+}
+
+export function docValidate(value: unknown): Doc {
+  const object = objectValidate(value);
+  stringValidate(object.title);
+  stringValidate(object.subtitle);
+  objectValidate(object.body);
+  objectValidate(object.audience);
+  return value as Doc;
+}
